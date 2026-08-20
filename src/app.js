d6e9b8e20d9dea1e5d4b2b1205100353be32dd56
David Casas Arenas <cem20903@gmail.com>
2026-08-20 18:17:38 +0200

    Remove solutions

--- src/app.js
+++ src/app.js
@@ -4,18 +4,15 @@ const app = express();
 app.use(express.json());
 
 // TODO: importar el router de artículos
-// const articlesRouter = require('./routes/articles');
+
 
 // TODO: montar el router en /articles
-// app.use('/articles', articlesRouter);
+
 
 // TODO: importar y usar mongooseErrorHandler antes de errorHandler
-// const mongooseErrorHandler = require('./middlewares/mongooseErrorHandler');
 
 // TODO: importar y usar errorHandler
-// const errorHandler = require('./middlewares/errorHandler');
 
-// app.use(mongooseErrorHandler);
-// app.use(errorHandler);
+
 
 module.exports = app;
